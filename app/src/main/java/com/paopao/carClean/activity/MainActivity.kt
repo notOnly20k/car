2c7f43b93caa146773f9cb574721c4d3fb56ba2a
cz <1552916842@qq.com>
2019-05-05 23:24:10 +0800

    ssss

--- app/src/main/java/com/paopao/carClean/activity/MainActivity.kt
+++ app/src/main/java/com/paopao/carClean/activity/MainActivity.kt
@@ -3,7 +3,6 @@ package com.paopao.carClean.activity
 import android.os.Bundle
 import com.paopao.carClean.R
 import com.paopao.carClean.activity.base.BaseActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : BaseActivity() {
